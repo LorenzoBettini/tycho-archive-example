fdbd8d84ad7578be2dceeea66673da9be0d2aa18
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-03-18 19:06:33 +0100

    require milestone argument

--- org.example.parent/set-milestone-poms.sh
+++ org.example.parent/set-milestone-poms.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+    echo "No arguments provided: pass the milestone, e.g., M1"
+    exit 1
+fi
+
 mvn \
   build-helper:parse-version \
   versions:set \
-  -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.M1
+  -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$1
