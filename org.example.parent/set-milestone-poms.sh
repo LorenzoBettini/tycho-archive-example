251f27c1109427f07eab5f056ffc7e78d3dc57d1
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-03-18 19:19:05 +0100

    -DgenerateBackupPoms=false

--- org.example.parent/set-milestone-poms.sh
+++ org.example.parent/set-milestone-poms.sh
@@ -8,4 +8,5 @@ fi
 mvn \
   build-helper:parse-version \
   versions:set \
+  -DgenerateBackupPoms=false \
   -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.$1
