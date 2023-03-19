a1f848c3cb139df49fdb21a1ef5388abb955f012
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-03-19 14:20:45 +0100

    initial tests

--- org.example.plugin1.tests/src/org/example/tests/HelloVersionTest.java
+++ org.example.plugin1.tests/src/org/example/tests/HelloVersionTest.java
@@ -1,9 +1,15 @@
 package org.example.tests;
 
+import static org.junit.Assert.assertTrue;
+
 import org.example.HelloVersion;
 import org.junit.Test;
 
-
+/**
+ * @author Lorenzo Bettini
+ *
+ * This must be run with tycho-surefire-plugin
+ */
 public class HelloVersionTest {
 
 	@Test
@@ -13,6 +19,13 @@ public class HelloVersionTest {
 
 		System.out.println("Bundle-Version: " + bundleVersion);
 		System.out.println("Maven-Version: " + mavenVersion);
+
+		String expectedPrefixVersion = "1.0.0";
+		String expectedPrefixTimestamp = "v20";
+		assertTrue("expected starting with " + expectedPrefixVersion,
+			bundleVersion.startsWith(expectedPrefixVersion));
+		assertTrue("expected containing with timestamp " + expectedPrefixTimestamp,
+			bundleVersion.contains(expectedPrefixTimestamp));
 	}
 
 }
