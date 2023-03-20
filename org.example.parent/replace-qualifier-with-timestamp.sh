6900fe997fc9ae9fc7596728c58fb880ac463322
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-03-20 11:08:39 +0100

    run single goals in the script

--- org.example.parent/replace-qualifier-with-timestamp.sh
+++ org.example.parent/replace-qualifier-with-timestamp.sh
@@ -2,4 +2,8 @@
 
 mvn \
   -Preplace-qualifier-with-timestamp \
-  validate
+  build-helper:parse-version \
+  build-helper:timestamp-property@timestamp \
+  org.eclipse.tycho:tycho-versions-plugin:set-version \
+  -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}.\${computedTimestamp} \
+  -DgenerateBackupPoms=false
